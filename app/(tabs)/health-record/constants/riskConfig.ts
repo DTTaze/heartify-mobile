0d22315787467048ffc0ec9b6c0d740bdd8b5549
Dang Thanh Tai <dtai668862@gmail.com>
2026-02-07 22:27:16 +0700

    feat: enhance HeartRiskIndex component with risk levels and improved styling

--- app/(tabs)/health-record/constants/riskConfig.ts
+++ app/(tabs)/health-record/constants/riskConfig.ts
@@ -1,4 +1,7 @@
-import { HealthRiskLevel } from '@/app/(tabs)/health-record/types/health';
+import {
+  HealthRiskLevel,
+  RiskLevel,
+} from '@/app/(tabs)/health-record/types/health';
 
 export const RISK_CONFIG: Record<
   HealthRiskLevel,
@@ -29,3 +32,46 @@ export const RISK_CONFIG: Record<
     borderColor: '#EB5757',
   },
 };
+
+export const RISK_LEVELS: RiskLevel[] = [
+  {
+    min: 0,
+    max: 10,
+    label: 'Low Risk',
+    feeling: 'Feeling Good',
+    heartColor: '#4DAB75',
+    bgColor: '#D3EADD',
+    borderColor: '#A6D5BA',
+    dotColor: '#009966',
+  },
+  {
+    min: 10,
+    max: 15,
+    label: 'Moderate Risk',
+    feeling: 'A Bit Off',
+    heartColor: '#FEEF77',
+    bgColor: '#FFFCE8',
+    borderColor: '#FEE41C',
+    dotColor: '#FEE41C',
+  },
+  {
+    min: 15,
+    max: 30,
+    label: 'High Risk',
+    feeling: 'Could Be Better',
+    heartColor: '#FFD98B',
+    bgColor: '#FFECC5',
+    borderColor: '#FFD06E',
+    dotColor: '#FFD06E',
+  },
+  {
+    min: 30,
+    max: Infinity,
+    label: 'Very High Risk',
+    feeling: 'Needs Support',
+    heartColor: '#EB5757',
+    bgColor: '#FBDDDD',
+    borderColor: '#EB5757',
+    dotColor: '#EB5757',
+  },
+];
