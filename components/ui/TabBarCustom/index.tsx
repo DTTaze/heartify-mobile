7327d6527a876b8327be33f883938c6952040901
kienle0108 <thatbatngo0108@gmail.com>
2026-02-08 10:56:55 +0700

    feat: introduce custom tab bar component for bottom navigation.

--- components/ui/TabBarCustom/index.tsx
+++ components/ui/TabBarCustom/index.tsx
@@ -17,7 +17,7 @@ interface TabItem {
 // 2. Mảng dữ liệu Tabs
 const TABS: TabItem[] = [
   { name: 'index', label: 'Home', icon: Home },
-  { name: 'health', label: 'Health', icon: Heart },
+  { name: 'health-record', label: 'Health', icon: Heart },
   { name: 'record-details', label: 'Camera', icon: Camera, isSpecial: true },
   { name: 'discover', label: 'Discover', icon: Book },
   // { name: 'health-record', label: 'Records', icon: Book },
@@ -28,7 +28,7 @@ const TAB_PATHS = {
   index: '/(tabs)',
   discover: '/(tabs)/discover',
   profile: '/(tabs)/profile',
-  health: '/(auth)/sign-up/measurements',
+  'health-record': '/(tabs)/health-record',
   'record-details': '/(tabs)/record-details',
 } as const;
 
